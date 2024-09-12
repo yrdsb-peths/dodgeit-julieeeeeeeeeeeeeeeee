e7b2159b467853b9750d71a576cbdce653e48ddb
julieeeeeeeeeeeeeeeee <341081727@gapps.yrdsb.ca>
2024-09-12 16:47:13 -0400

    Reset bomb

--- Bomb.java
+++ Bomb.java
@@ -6,5 +6,23 @@ public class Bomb extends Actor
     {
         // Add your action code here.
         move(-3);
+        
+        if(getX() <= 0)
+        {
+            resetBomb();
+        }
+    }
+    
+    public void resetBomb()
+    {
+        int num = Greenfoot.getRandomNumber(2);
+        if(num == 0)
+        {
+            setLocation(600, 100);
+        }
+        else
+        {
+            setLocation(600, 300);
+        }
     }
 }
